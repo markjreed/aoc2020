7fd5839e27b340e706af07aa72fb45ddebb90d33
Mark J. Reed <markjreed@gmail.com>
2020-12-15 23:25:38 -0500

    use <- consistently for assignment

--- day15/solve15.r
+++ day15/solve15.r
@@ -4,15 +4,15 @@ option_list = list(
   make_option(c("-v", "--verbose"), action="store_true", type="logical", default=FALSE, help="Show turn by turn"),
   make_option(c("-t", "--turn"), type="integer", default=2020, help="Turn to stop on", metavar="number"))
 
-opt_parser = OptionParser(option_list = option_list)
-opt = parse_args(opt_parser, positional_arguments = TRUE)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser, positional_arguments = TRUE)
 
-starting_list = strsplit(opt$args[[1]], ",", fixed=TRUE)[[1]]
-last = starting_list[[length(starting_list)]]
+starting_list <- strsplit(opt$args[[1]], ",", fixed=TRUE)[[1]]
+last <- starting_list[[length(starting_list)]]
 starting_list <- starting_list[-length(starting_list)]
-turn = length(starting_list) + 1
+turn <- length(starting_list) + 1
 
-spoken = new.env(hash=TRUE, parent=emptyenv())
+spoken <- new.env(hash=TRUE, parent=emptyenv())
 for (i in seq_along(starting_list)) {
   if (opt$options$verbose) {
     cat(c("Turn ", i, ": ", starting_list[[i]], "\n"))
@@ -24,8 +24,8 @@ while (turn < opt$options$turn) {
   if (opt$options$verbose) {
     cat(c("Turn ", turn, ": ", last, "\n"))
   }
-  say = 0
-  key = toString(last)
+  say <- 0
+  key <- toString(last)
   if (exists(key, spoken)) {
     say <- turn - spoken[[key]]
   }
