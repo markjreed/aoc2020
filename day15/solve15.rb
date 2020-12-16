3785d580597f534cf636d5475b17c2950b04f6a8
Mark J. Reed <markjreed@gmail.com>
2020-12-15 20:35:36 -0500

    formatting, fix verbose output

--- day15/solve15.rb
+++ day15/solve15.rb
@@ -2,25 +2,35 @@
 
 def main(numbers, position=2020, verbose=False)
   starting_list = numbers.split(',').map(&:to_i)
-  spoken = starting_list[0..-2].each_with_index.map.to_h
-  last = starting_list[-1]
-  turn = starting_list.length - 1
-  while turn < position -1 do 
-    if spoken.include? last then
+  last = starting_list.pop
+
+  if verbose
+    starting_list.each_with_index do |n, i|
+      puts "Turn #{i+1}: #{n}"
+    end
+  end
+
+  turn = starting_list.length
+  spoken = starting_list.each_with_index.to_h
+
+  while turn < position - 1
+
+    puts "Turn #{turn+1}: #{last}" if verbose
+
+    say = 0
+    if spoken.include?(last)
       say = turn - spoken[last]
-    else
-      say = 0
     end
     spoken[last] = turn
     last = say
-    turn = turn + 1
+    turn += 1
   end
 
   print("Turn #{turn+1}: ") if verbose;
   puts(last)
 end
 
-if __FILE__ == $0 
+if __FILE__ == $0
   position=2020
   verbose=false
   while ARGV[0] =~ /^-/ do
